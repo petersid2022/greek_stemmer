bd42c13925196b8f889a8cd66e59d864da0fcb98
Peter Sideris <petersid2022@gmail.com>
2023-06-15 16:56:22 +0300

    Added Step 1

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/petersid2022/greek_stemmer_go
 
 go 1.20
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require gopkg.in/yaml.v2 v2.4.0
--- lib/greek_stemmer.go
+++ lib/greek_stemmer.go
@@ -2,77 +2,111 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"regexp"
 	"strings"
 	"unicode/utf8"
-
-	"gopkg.in/yaml.v3"
 )
 
-func loadsettings(x string) []string {
-    config_path, err := os.ReadFile("../config/stemmer.yml")
-    if err != nil {
-        log.Fatal(err)
-    }
-    config := make(map[interface{}]interface{})
-    err2 := yaml.Unmarshal(config_path, &config)
-    if err2 != nil {
-        log.Fatal(err2)
-    }
+type Config struct {
+	Step1Exceptions map[string]string "json:\"step_1_exceptions\""
+	Step0Exceptions map[string]string "json:\"step_0_exceptions\""
+	ProtectedWords  []string          "json:\"protected_words\""
+}
 
-    out := []string{}
-    for _, item := range config[x].([]interface{}) {
-        out = append(out, item.(string))
-    }
-    return out 
+func parseConfigFile(filepath string) (Config, error) {
+	jsonFile, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return Config{}, fmt.Errorf("Failed to read the JSON file: %v", err)
+	}
+	var config Config
+	err = json.Unmarshal(jsonFile, &config)
+	if err != nil {
+		return Config{}, fmt.Errorf("Failed to unmarshal the JSON data: %v", err)
+	}
+	return config, err
 }
 
 func isgreek(x string) bool {
-    alphabet :=  regexp.MustCompile("^[ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ]+$")
-    return alphabet.MatchString(x)
+	alphabet := regexp.MustCompile("^[ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ]+$")
+	return alphabet.MatchString(x)
 }
 
 func contains(s []string, str string) bool {
-    for _, v := range s {
-        if v == str {
-            return true
-        }
-    }
-
-    return false 
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
 
-func main(){
+func getUserInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Input: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("Failed to read input: %v", err)
+	}
+	return strings.ReplaceAll(input, "\n", "")
+}
 
-    // Getting user input
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Println("word: ")
-    input, err := reader.ReadString('\n')
-    if err != nil {
-        os.Exit(33)
-    }
-    word := strings.ReplaceAll(input, "\n", "")
+func step1Regex(step1Exceptions map[string]string) (*regexp.Regexp, error) {
+	suffixes := make([]string, 0, len(step1Exceptions))
+	for suffix := range step1Exceptions {
+		suffixes = append(suffixes, suffix)
+	}
+	pattern := "(.*)(?:" + strings.Join(suffixes, "|") + ")$"
+	return regexp.Compile(pattern)
+}
 
-    // Setting environment variables
-    protected_words := loadsettings("protected_words")
-    //step_0_exceptions := loadsettings("step_0_exceptions")
-    stem := strings.Clone(word)
+func main() {
+	config, err := parseConfigFile("config/config.json")
+	if err != nil {
+		log.Fatalf("Failed to parse config file: %v", err)
+	}
 
-    // Step 0
-    if utf8.RuneCountInString(stem) < 3 || !isgreek(stem) {
-        fmt.Println(stem)
-    }     
+	word := getUserInput()
+	stem := strings.Clone(word)
 
-    if contains(protected_words, stem){
-        fmt.Println(stem)
-    }
+	if !isgreek(word) {
+		log.Fatalf("Word is not valid: %v", word)
+	}
 
-    // Step 1
-    
+	protected := config.ProtectedWords
+	step0 := config.Step0Exceptions
+	step1 := config.Step1Exceptions
+
+    // If the word has less than 3 characters or is protected, return the word
+	if utf8.RuneCountInString(stem) < 3 || contains(protected, stem) {
+		fmt.Println("Stemmed word:", stem)
+		os.Exit(0)
+	}
+
+    // Step 0 
+	if val, ok := step0[stem]; ok {
+		fmt.Println("Stemmed word:", val)
+		os.Exit(0)
+	}
+
+	// Step 1
+	step1Pattern, err := step1Regex(step1)
+	if err != nil {
+		log.Fatalf("Failed to compile step1 regular expression: %v", err)
+	}
+
+	stem = step1Pattern.ReplaceAllStringFunc(stem, func(match string) string {
+		suffix := match
+		replacement := step1[suffix]
+		return replacement
+	})
+
+    // Step 2
 
 
-
+	fmt.Println("Stemmed word: ", stem)
 }
