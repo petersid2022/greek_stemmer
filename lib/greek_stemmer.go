c00c0353d5c1cba0b658f239a3bcd68d3ba97884
Peter Sideris <petersid2022@gmail.com>
2023-06-26 01:41:17 +0300

    Added step 2 of the algorithm

--- lib/greek_stemmer.go
+++ lib/greek_stemmer.go
@@ -1,4 +1,4 @@
-package main
+package main 
 
 import (
 	"bufio"
@@ -9,7 +9,6 @@ import (
 	"os"
 	"regexp"
 	"strings"
-	"unicode/utf8"
 )
 
 type Config struct {
@@ -18,17 +17,17 @@ type Config struct {
 	ProtectedWords  []string          "json:\"protected_words\""
 }
 
-func parseConfigFile(filepath string) (Config, error) {
+func parseConfigFile(filepath string) Config {
 	jsonFile, err := ioutil.ReadFile(filepath)
 	if err != nil {
-		return Config{}, fmt.Errorf("Failed to read the JSON file: %v", err)
+		log.Fatalf("Failed to read the JSON file: %v", err)
 	}
 	var config Config
 	err = json.Unmarshal(jsonFile, &config)
 	if err != nil {
-		return Config{}, fmt.Errorf("Failed to unmarshal the JSON data: %v", err)
+		log.Fatalf("Failed to unmarshal the JSON data: %v", err)
 	}
-	return config, err
+	return config
 }
 
 func isgreek(x string) bool {
@@ -36,9 +35,9 @@ func isgreek(x string) bool {
 	return alphabet.MatchString(x)
 }
 
-func contains(s []string, str string) bool {
-	for _, v := range s {
-		if v == str {
+func contains(word string, pool []string) bool {
+	for _, w := range pool {
+		if word == w {
 			return true
 		}
 	}
@@ -47,7 +46,7 @@ func contains(s []string, str string) bool {
 
 func getUserInput() string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Input: ")
+    fmt.Print("Input Greek capital letters only! : ")
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatalf("Failed to read input: %v", err)
@@ -60,37 +59,36 @@ func step1Regex(step1Exceptions map[string]string) (*regexp.Regexp, error) {
 	for suffix := range step1Exceptions {
 		suffixes = append(suffixes, suffix)
 	}
-	pattern := "(.*)(?:" + strings.Join(suffixes, "|") + ")$"
-	return regexp.Compile(pattern)
+	step2A_pattern := "(.*)(?:" + strings.Join(suffixes, "|") + ")$"
+	return regexp.Compile(step2A_pattern)
+}
+
+func getMatchesFromInput(pattern string, stem string) []string {
+    r := regexp.MustCompile(pattern)
+	matches := r.FindStringSubmatch(stem)
+    return matches
 }
 
 func main() {
-	config, err := parseConfigFile("config/config.json")
-	if err != nil {
-		log.Fatalf("Failed to parse config file: %v", err)
-	}
-
+	config := parseConfigFile("config/config.json")
 	word := getUserInput()
-	stem := strings.Clone(word)
+    if len(word) < 3 && isgreek(word){
+        fmt.Println(word)
+        os.Exit(0)
+    }
 
-	if !isgreek(word) {
-		log.Fatalf("Word is not valid: %v", word)
+    stem := strings.Clone(word)
+    protected := config.ProtectedWords
+    step1 := config.Step1Exceptions
+    step0 := config.Step0Exceptions
+
+	if contains(stem, protected) || !isgreek(stem) {
+		stem = "input word is protected or is not greek!"
 	}
 
-	protected := config.ProtectedWords
-	step0 := config.Step0Exceptions
-	step1 := config.Step1Exceptions
-
-    // If the word has less than 3 characters or is protected, return the word
-	if utf8.RuneCountInString(stem) < 3 || contains(protected, stem) {
-		fmt.Println("Stemmed word:", stem)
-		os.Exit(0)
-	}
-
-    // Step 0 
+	// Step 0
 	if val, ok := step0[stem]; ok {
-		fmt.Println("Stemmed word:", val)
-		os.Exit(0)
+		stem = val
 	}
 
 	// Step 1
@@ -98,15 +96,63 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to compile step1 regular expression: %v", err)
 	}
-
 	stem = step1Pattern.ReplaceAllStringFunc(stem, func(match string) string {
 		suffix := match
 		replacement := step1[suffix]
 		return replacement
 	})
 
-    // Step 2
+	// Step 2a
+	step2A_pattern := "^(.+?)(ΑΔΕΣ|ΑΔΩΝ)$"
+    matches_2a := getMatchesFromInput(step2A_pattern,stem)
+	if len(matches_2a) > 0 {
+		st := matches_2a[1]
+		if matched := regexp.MustCompile(`((ΟΚ|ΜΑΜ|ΜΑΝ|ΜΠΑΜΠ|ΠΑΤΕΡ|ΓΙΑΓΙ|ΝΤΑΝΤ|ΚΥΡ|ΘΕΙ|ΠΕΘΕΡ|ΜΟΥΣΑΜ|ΚΑΠΛΑΜ|ΠΑΡ|ΨΑΡ|ΤΖΟΥΡ|ΤΑΜΠΟΥΡ))$`).MatchString(st); matched {
+			stem = st + "ΑΔ"
+		}
+	}
 
+    // Step 2b
+    step2B_pattern := "^(.+?)(ΕΔΕΣ|ΕΔΩΝ)$" 
+    matches_2b := getMatchesFromInput(step2B_pattern,stem)
+	if len(matches_2b) > 0 {
+		st := matches_2b[1]
+		if matched := regexp.MustCompile(`((ΟΠ|ΙΠ|ΕΜΠ|ΥΠ|ΓΗΠ|ΔΑΠ|ΚΡΑΣΠ|ΜΙΛ))$`).MatchString(st); matched {
+			stem = st + "EΔ"
+		}
+	}
 
-	fmt.Println("Stemmed word: ", stem)
+    // Step 2c
+    step2C_pattern := "^(.+?)(ΟΥΔΕΣ|ΟΥΔΩΝ)$" 
+    matches_2c := getMatchesFromInput(step2C_pattern,stem)
+	if len(matches_2c) > 0 {
+		st := matches_2c[1]
+		if matched := regexp.MustCompile(`((ΑΡΚ|ΚΑΛΙΑΚ|ΠΕΤΑΛ|ΛΙΧ|ΠΛΕΞ|ΣΚ|Σ|ΦΛ|ΦΡ|ΒΕΛ|ΛΟΥΛ|ΧΝ|ΣΠ|ΤΡΑΓ|ΦΕ))$`).MatchString(st); matched {
+			stem = st + "ΟΥΔ"
+		}
+	}
+
+    // Step 2d
+    step2D_pattern := "^(.+?)(ΕΩΣ|ΕΩΝ|ΕΑΣ|ΕΑ)$" 
+    matches_2d := getMatchesFromInput(step2D_pattern,stem)
+	if len(matches_2d) > 0 {
+		st := matches_2d[1]
+		if matched := regexp.MustCompile(`(^(Θ|Δ|ΕΛ|ΓΑΛ|Ν|Π|ΙΔ|ΠΑΡ|ΣΤΕΡ|ΟΡΦ|ΑΝΔΡ|ΑΝΤΡ))$`).MatchString(st); matched {
+			stem = st + "Ε"
+		}
+	}
+
+    // Step 3a 
+    //step3A_pattern := "^(.+?)(ΕΙΟ|ΕΙΟΣ|ΕΙΟΙ|ΕΙΑ|ΕΙΑΣ|ΕΙΕΣ|ΕΙΟΥ|ΕΙΟΥΣ|ΕΙΩΝ)$"
+    //matches_3a := getMatchesFromInput(step3A_pattern, stem)
+    //st := matches_3a[1]
+    //if len(st) > 4 {
+    //    stem = st
+    //}
+
+    // Step 3b
+    //step3B_pattern := "^(.+?)(ΙΟΥΣ|ΙΑΣ|ΙΕΣ|ΙΟΣ|ΙΟΥ|ΙΟΙ|ΙΩΝ|ΙΟΝ|ΙΑ|ΙΟ)$"
+    //matches_3b := getMatchesFromInput(step3B_pattern, stem)
+
+    fmt.Println(stem)
 }
